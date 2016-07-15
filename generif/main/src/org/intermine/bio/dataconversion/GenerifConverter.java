2121060172528de7ec90da9f9c6f54a100e9c629
Vivek Krishnakumar <vkrishna@jcvi.org>
2016-07-14 20:46:39 -0400

    + Adds the pubmed-gene source and configures the entrez ID resolver + Adds the NCBI GeneRIF source and corresponding displayer + Adds the InterMine update-publications source

--- generif/main/src/org/intermine/bio/dataconversion/GenerifConverter.java
+++ generif/main/src/org/intermine/bio/dataconversion/GenerifConverter.java
@@ -13,14 +13,18 @@ package org.intermine.bio.dataconversion;
 import java.io.File;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
 import org.apache.tools.ant.BuildException;
 import org.intermine.dataconversion.ItemWriter;
 import org.intermine.metadata.Model;
+import org.intermine.metadata.StringUtil;
 import org.intermine.objectstore.ObjectStoreException;
 import org.intermine.util.FormattedTextParser;
 import org.intermine.xml.full.Item;
@@ -36,10 +40,10 @@ public class GenerifConverter extends BioFileConverter
     private static final String DATASET_TITLE = "GeneRIF";
     private static final String DATA_SOURCE_NAME = "NCBI";
 
-    // for the moment dealing only with ath
-    private Item org;
-    private static final String ATH_TAXID = "3702";
+    private IdResolver rslv;
+    private Set<String> taxonIds = new HashSet<String>();
 
+    private Map<String, String> orgItems = new HashMap<String, String>();
     private Map<String, String> pubItems = new HashMap<String, String>();
     private Map<String, String> geneItems = new HashMap<String, String>();
 
@@ -52,7 +56,19 @@ public class GenerifConverter extends BioFileConverter
     public GenerifConverter(ItemWriter writer, Model model)
         throws ObjectStoreException {
         super(writer, model, DATA_SOURCE_NAME, DATASET_TITLE);
-        createOrganismItem();
+    }
+
+    /**
+     * Sets the list of taxonIds that should be processed.  All genes will be loaded.
+     *
+     * @param taxonIds a space-separated list of taxonIds
+     */
+    public void setGenerifOrganisms(String taxonIds) throws ObjectStoreException {
+        this.taxonIds = new HashSet<String>(Arrays.asList(StringUtil.split(taxonIds, " ")));
+        LOG.info("Setting list of organisms to " + taxonIds);
+        for (String taxonId : this.taxonIds) {
+            createOrganismItem(taxonId);
+        }
     }
 
     /**
@@ -64,7 +80,7 @@ public class GenerifConverter extends BioFileConverter
         File currentFile = getCurrentFile();
 
         if ("generifs_basic".equals(currentFile.getName())) {
-            processFile(reader, org);
+            processFile(reader);
         } else {
             LOG.info("WWSS skipping file: " + currentFile.getName());
             //            throw new IllegalArgumentException("Unexpected file: "
@@ -90,7 +106,7 @@ public class GenerifConverter extends BioFileConverter
      *
      *
      */
-    private void processFile(Reader reader, Item organism)
+    private void processFile(Reader reader)
         throws IOException, ObjectStoreException {
         Iterator<?> tsvIter;
         try {
@@ -98,7 +114,16 @@ public class GenerifConverter extends BioFileConverter
         } catch (Exception e) {
             throw new BuildException("cannot parse file: " + getCurrentFile(), e);
         }
-        IdResolver athResolver = IdResolverService.getIdResolverByOrganism("3702");
+
+        if (taxonIds.isEmpty()) {
+            LOG.warn("generif.organisms property not set in project XML file");
+        }
+
+        //Create id resolver
+        if (rslv == null) {
+            rslv = IdResolverService.getIdResolverByOrganism(taxonIds);
+        }
+
         String pid = null;
 
         int lineNumber = 0;
@@ -119,12 +144,11 @@ public class GenerifConverter extends BioFileConverter
             String timeStamp = line[3];
             String annotation = line [4];
 
-            // dealing only with ATH for now
-            if (!taxid.equalsIgnoreCase(ATH_TAXID)) {
+            if (!taxonIds.contains(taxid)) {
                 continue;
             }
 
-            int resCount = athResolver.countResolutions(taxid, geneId);
+            int resCount = rslv.countResolutions(taxid, geneId);
             if (resCount != 1) {
                 LOG.info("RESOLVER: failed to resolve gene to one identifier, ignoring gene: "
                         + geneId + " count: " + resCount);
@@ -139,7 +163,7 @@ public class GenerifConverter extends BioFileConverter
             //          }
 
 
-            pid = athResolver.resolveId(taxid, geneId).iterator().next();
+            pid = rslv.resolveId(taxid, geneId).iterator().next();
             LOG.info("READING " + taxid + ": " + pid + "<->" + geneId + "|" + pubMedId + "|"
                     + timeStamp + "--" + annotation);
 
@@ -147,7 +171,7 @@ public class GenerifConverter extends BioFileConverter
             createBioEntity(pid, "Gene");
             createPublication(pubMedId);
             ann.setReference("gene", geneItems.get(pid));
-            ann.setReference("organism", organism);
+            ann.setReference("organism", orgItems.get(taxid));
             ann.setReference("publication", pubItems.get(pubMedId));
             store(ann);
 
@@ -232,12 +256,16 @@ public class GenerifConverter extends BioFileConverter
     /**
      * Create and store a organism item on the first time called.
      *
+     * @param taxonId the taxon ID
      * @throws ObjectStoreException os
      */
-    protected void createOrganismItem() throws ObjectStoreException {
-        org = createItem("Organism");
-        org.setAttribute("taxonId", ATH_TAXID);
-        store(org);
+    private void createOrganismItem(String taxonId) throws ObjectStoreException {
+        if (!orgItems.containsKey(taxonId)) {
+            Item org = createItem("Organism");
+            org.setAttribute("taxonId", taxonId);
+            store(org);
+            orgItems.put(taxonId, org.getIdentifier());
+        }
     }
 
 }
